5658be04c7b5c6bb6521dedf1a051c1f8c4538d4
dieguischa <dfchacona@unal.edu.co>
2022-01-14 18:08:30 -0500

    Creation and edition of projects & dedications

---
+++ src/services/DedicationService.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { API_URL } from '../consts';
+
+export default async function getAllDedications() {
+    return axios.get(API_URL+"api/v1/dedications", {
+      headers: {
+              "Authorization": window.sessionStorage.getItem("auth_token"),
+        }
+    })
+  }
+
+export async function editDedication(id, data) {
+    return axios.patch(
+        API_URL+"api/v1/dedications/"+id,
+        data,
+        {
+            headers: {
+                "Authorization": window.sessionStorage.getItem("auth_token"),
+            }
+        }
+    )
+}
+
+export async function createDedication(data) {
+    return axios.post(
+        API_URL+"api/v1/dedications",
+        data,
+        {
+            headers: {
+                "Authorization": window.sessionStorage.getItem("auth_token"),
+            }
+        }
+    )
+}
---
+++ src/services/ProjectsService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { API_URL } from '../consts';
+
+export default async function getAllProjects() {
+  return axios.get(API_URL+"api/v1/projects", {
+    headers: {
+            "Authorization": window.sessionStorage.getItem("auth_token"),
+        }
+    })
+}
